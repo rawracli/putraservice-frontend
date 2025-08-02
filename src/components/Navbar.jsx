b007b9e3ba104841136728fd4e4f05fa598d7ca2
Muhammad Rafli <149589644+RaflIsLife@users.noreply.github.com>
2025-08-02 23:22:19 +0700

    Add changes onScroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,46 @@
 import { Link, NavLink } from "react-router-dom";
 import DropDown from "../assets/dropdown.png";
 import Logo from "../assets/logo.svg";
+import { useEffect, useState } from "react";
 
-function navbar() {
+function Navbar() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isOnTop, setIsOnTop] = useState(true);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+    const controlNavbar = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > 0) {
+        setIsOnTop(false);
+      } else {
+        setIsOnTop(true);
+      }
+      if (currentScrollY > lastScrollY && currentScrollY > 150) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      lastScrollY = currentScrollY;
+    };
+
+    window.addEventListener("scroll", controlNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, []);
+
   const handleDropdown = () => {
     const dropdown = document.getElementById("dropdown");
     dropdown.classList.toggle("hidden");
   };
   return (
-    <div className="z-20 fixed w-full flex items-center justify-center h-23 from-black/18 to-[#9C9C9C]/18 bg-gradient-to-r text-white font-semibold">
+    <div
+      className={`z-20 backdrop- fixed w-full flex items-center justify-center transition-all duration-300 font-semibold ${
+        isVisible ? "translate-y-0" : "-translate-y-[110%]"
+      } ${isOnTop ? "text-white from-black/18 to-[#9C9C9C]/18 bg-gradient-to-r h-23" : "bg-white h-19 shadow-sm shadow-white"}`}
+    >
       <div className="flex items-center justify-center w-full mx-22">
         <div className="w-full h-full flex items-center">
           <img src={Logo} alt="Logo" className="w-16 " />
@@ -18,34 +50,53 @@ function navbar() {
           <NavLink
             to="/"
             className={({ isActive }) =>
-              isActive ? "border-[#A42619] border-b-2 relative" : "relative group"
+              isActive
+                ? "border-[#A42619] border-b-2 relative"
+                : "relative group"
             }
           >
             Home
             <div className="absolute border-b-2 w-0 group-hover:w-full ease-out duration-100 border-red-600 transition-all"></div>
           </NavLink>
-          <NavLink to="/about" className={({isActive}) => isActive ? "border-[#A42619] border-b-2 relative" : "relative group"}>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive
+                ? "border-[#A42619] border-b-2 relative"
+                : "relative group"
+            }
+          >
             Tentang
             <div className="absolute border-b-2 w-0 group-hover:w-full ease-out duration-100 border-red-600 transition-all"></div>
           </NavLink>
-          <NavLink to="/service" className={({isActive}) => isActive ? "border-[#A42619] border-b-2 relative" : "relative group"}>
+          <NavLink
+            to="/service"
+            className={({ isActive }) =>
+              isActive
+                ? "border-[#A42619] border-b-2 relative"
+                : "relative group"
+            }
+          >
             Layanan
             <div className="absolute border-b-2 w-0 group-hover:w-full ease-out duration-100 border-red-600 transition-all"></div>
           </NavLink>
-          <div className="flex relative group" onClick={handleDropdown}>
+          <div
+            className="flex flex-col relative group"
+            onClick={handleDropdown}
+          >
             <div className="py-4 flex items-center">
               <button>Lainnya</button>
               <img src={DropDown} alt="" className="w-3 ml-2 mt-1" />
             </div>
             <div className="invisible absolute group-hover:visible flex hover:scale-100 transition-all top-12 left-0 px-3 pb-2">
               <div
-                className="divide-y-[1.5px] divide-white shadow-md"
+                className={`divide-y-[1.5px] shadow-md ${isOnTop ? "divide-white" : "divide-black"}`}
                 id="dropdown"
               >
-                <div className="py-2.5 px-4 bg-black/30 hover:bg-black/50 transition-all">
+                <div className={`py-2.5 px-4  transition-all ${isOnTop ? "hover:bg-black/30" : "bg-[#FFFBFB] hover:bg-gray-100"}`}>
                   <Link to="/contact">Kontak</Link>
                 </div>
-                <div className="py-2.5 px-4 bg-black/30 hover:bg-black/50 transition-all">
+                <div className={`py-2.5 px-4  transition-all ${isOnTop ? "hover:bg-black/30" : "bg-[#FFFBFB] hover:bg-gray-100"}`}>
                   <Link to="/documentation">Dokumentasi</Link>
                 </div>
               </div>
@@ -53,7 +104,7 @@ function navbar() {
           </div>
         </div>
         <div className="w-full h-full flex items-center justify-end">
-          <Link className="bg-[#A30F00] hover:bg-[#730B00] transition-all py-2 px-5 rounded-lg active:bg-[#600000]">
+          <Link className="bg-[#A30F00] text-white hover:bg-[#730B00] transition-all py-2 px-5 rounded-lg active:bg-[#600000]">
             Pesan Sekarang
           </Link>
         </div>
@@ -62,4 +113,4 @@ function navbar() {
   );
 }
 
-export default navbar;
+export default Navbar;
