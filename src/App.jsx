0525672324ada75f61a69efc0f6e51f826bef0fc
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-08-07 00:34:01 +0700

    Add Layanan section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+// TODO: Reorganize everything
 // import { useState } from 'react'
 import "./App.css";
 import { Link } from "react-router-dom";
 import Review from "./components/Review";
 import Card from "./components/whyChooseUse/Card.jsx";
 import DropDown from "./components/faq/DropDown.jsx";
+import Layanan from "./components/Home/Layanan.jsx";
 
 import Banner from "./assets/banner.jpg";
 import AcWall from "./assets/ac wall.png";
@@ -28,6 +30,12 @@ import KimiaFarma from "./assets/Logo/kimia farma.png";
 import Pegadaian from "./assets/Logo/pegadaian.png";
 import SCG from "./assets/Logo/scg.png";
 import Smartfren from "./assets/Logo/smartfren.png";
+import Ac from "./assets/Home/Layanan/ac.png";
+import Bg from "./assets/Home/Layanan/bg.png";
+import Bersih from "./assets/Home/Layanan/Logo/bersih.png";
+import Ganti from "./assets/Home/Layanan/Logo/ganti.png";
+import Instalasi from "./assets/Home/Layanan/Logo/instalasi.png";
+import Service from "./assets/Home/Layanan/Logo/service.png";
 
 import { RiArrowDropDownLine } from "react-icons/ri";
 
@@ -189,20 +197,20 @@ function App() {
           <h2 className="font-source-serif text-[2.8rem] font-semibold text-center pb-13">
             Mereka yang Telah Mempercayai Layanan Kami
           </h2>
-            <div className="grid grid-cols-5 gap-2 mx-55">
-          <div className="h-25 flex items-center">
-            <img src={Gourmet} alt="" />
-          </div>
-          <div className="h-25 flex items-center">
-            <img src={Gramedia} alt="" />
-          </div>
+          <div className="grid grid-cols-5 gap-2 mx-55">
+            <div className="h-25 flex items-center">
+              <img src={Gourmet} alt="" />
+            </div>
+            <div className="h-25 flex items-center">
+              <img src={Gramedia} alt="" />
+            </div>
             <div className="h-25 flex items-center">
               <img src={AmorCake} alt="" />
             </div>
             <div className="h-25 flex items-center">
               <img src={BPJS} alt="" />
             </div>
-              <div className="h-25 flex items-center">
+            <div className="h-25 flex items-center">
               <img src={Smartfren} alt="" />
             </div>
             <div className="h-25 flex items-center justify-center">
@@ -220,7 +228,59 @@ function App() {
             <div className="h-25 flex items-center pb-7">
               <img src={KimiaFarma} alt="" />
             </div>
-          
+          </div>
+        </div>
+      </div>
+
+      {/* layanan */}
+      <div className="flex flex-col items-center min-h-[120vh]">
+        <div className="font-semibold text-center pt-20">
+          <h3 className="text-xl text-[#A4161A]">Jasa</h3>
+          <h2 className="text-4xl tracking-wide">Layanan Kami</h2>
+        </div>
+        <div className="relative flex flex-1 w-full">
+          <div className="w-[calc(50%-28px)] flex items-end">
+            <img
+              src={Ac}
+              alt=""
+              className="relative pl-12 z-10 w-[calc(100%-14px)] translate-y-16"
+            />
+          </div>
+          <img src={Bg} alt="" className="absolute bottom-0 left-0 w-155" />
+          <div className="z-10 w-1/2 mt-16 flex flex-col gap-4 justify-evenly *:rounded-lg">
+            
+            <Layanan
+              image={Service}
+              title={"Service AC"}
+              description={
+                "Utamakan kenyamanan Anda dengan service AC cepat dan tepat. Dikerjakan oleh tim profesional, AC Anda kembali optimal dengan harga bersahabat"
+              }
+              className={"pl-0.5 pb-0.5"}
+            />
+            <Layanan
+              image={Bersih}
+              title={"Pembersihan AC"}
+              description={
+                "Bersihkan AC secara rutin untuk kinerja optimal. Tim kami siap membersihkan hingga ke sela-sela terdalam dengan cepat dan aman."
+              }
+              className={""}
+            />
+            <Layanan
+              image={Ganti}
+              title={"Penggantian Suku Cadang AC"}
+              description={
+                "Pergantian suku cadang profesional dengan pilihan sparepart terbaik. AC lebih dingin, hemat energi, dan awet."
+              }
+              className={""}
+            />
+            <Layanan
+              image={Instalasi}
+              title={"Instalasi AC Baru"}
+              description={
+                "Layanan instalasi AC baru cepat dan rapi. Dengan tim profesional, kami pastikan AC Anda terpasang dengan aman dan berfungsi optimal sejak awal."
+              }
+              className={""}
+            />
           </div>
         </div>
       </div>
---
+++ src/components/Home/Layanan.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const Layanan = ({image, title, description, className = ""}) => {
+  return (
+    <div className="bg-gradient-to-l from-[#E11500] via-[#A30F00] to-[#730B00] flex-1 flex">
+      <div className="items-center justify-center flex py-5 pl-15 pr-13">
+        <div className="bg-white size-20 rounded-full flex items-center justify-center">
+          <img src={image} alt="" className={`w-11 ${className}`} />
+        </div>
+      </div>
+      <div className="text-white flex flex-col w-[calc(100%-14rem)] relative">
+        <h4 className="text-[clamp(1rem,2vw,1.5rem)] font-bold pt-1.5">
+          {title}
+        </h4>
+        <p className="text-[clamp(6px,2vw,14px)] leading-snug tracking-wide font-light text-[#D1D0CF]">
+          {description}
+        </p>
+        <Link
+          to={"/layanan"}
+          className="text-[#BA181B] bg-white absolute bottom-3 right-0 text-xs font-semibold py-1.5 pl-4 pr-3 rounded-md"
+        >
+          Read More...
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Layanan;
