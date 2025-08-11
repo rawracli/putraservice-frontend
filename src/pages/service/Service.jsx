1e1d615f4b53c246179fa392175ccb8c2c2de8f3
Deergantara <mrdeerhardiansyah6@gmail.com>
2025-08-11 13:36:15 +0700

    Add service page and fix import

--- src/pages/service/Service.jsx
+++ src/pages/service/Service.jsx
@@ -1,7 +1,6 @@
+import CTASection from "./section/CTASection"
 function Service() {
-    return(<div className="min-h-screen flex justify-center items-center">
-      <h1 className="font-black text-7xl">Service Page</h1>
-    </div>)
+    return (<CTASection/>)
 }
 
 export default Service;
