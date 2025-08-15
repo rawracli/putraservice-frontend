f0a472fb5ddb1a1b6a68ca5e11ceaf3d03595d71
Muhammad Rafli <149589644+RaflIsLife@users.noreply.github.com>
2025-08-15 12:53:59 +0700

    Fix: Hero responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ function Hero({ title, backgroundImage, backgroundPosition, children, descriptio
     >
       <div className="bg-gradient-to-t to-70% from-[#BCBCBC] to-transparent opacity-40 absolute z-10 h-full w-full">
       </div>
-      <div className="z-20 sm:w-[54%] md:w-[44%] self-center sm:self-start mx-6 sm:ml-14 md:ml-20 pt-14">
-        <h1 className="text-white text-[2rem] sm:text-[2.2rem] md:text-[2.813rem] pb-5 tracking-[0] font-bold text-center">
+      <div className="z-20 self-center sm:self-start mx-6 sm:ml-14 md:ml-20 pt-14">
+        <h1 className="text-white sm:w-full text-[2rem] sm:text-[2.2rem] md:text-[2.813rem] pb-5 tracking-[0] font-bold">
           {title}
         </h1>
-        <p className="text-[#ADADAD] text-sm hidden sm:block">{description}</p>
+        <p className="text-[#ADADAD] sm:w-[54%] md:w-[60%] lg:w-[50%] xl:w-[45%] text-sm hidden sm:block">{description}</p>
       </div>
       {children}
     </div>
