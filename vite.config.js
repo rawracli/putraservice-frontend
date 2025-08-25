10e437b763e909bdfab4c9e5a8bb18d2f5a232b0
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-08-25 12:34:53 +0700

    Add lazy page load and module splitting

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,17 @@ export default defineConfig({
       open: true,
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1500, // naikin limit biar warning nggak terlalu rewel
+  },
   assetsInclude: ["**/*.glb"],
 });
