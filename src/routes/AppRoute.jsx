1382cf2dd1df25bdac5c09e4d20c5f9d44ffaf24
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-08-26 17:05:26 +0700

    Remove lazy page load and module splitting

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,26 +1,21 @@
-import {lazy, Suspense} from "react";
 import { Route, Routes } from "react-router-dom";
-import App from "../App"
-const About = lazy(() => import("../pages/about/About"))
-const Contact = lazy(() => import("../pages/contact/Contact"))
-const Service = lazy(() => import("../pages/service/Service"))
-const Documentation = lazy(() => import("../pages/documentation/Documentation"))
+import App from "../App";
+import About from "../pages/about/About";
+import Contact from "../pages/contact/Contact";
+import Service from "../pages/service/Service";
+import Documentation from "../pages/documentation/Documentation";
 
 function AppRoute() {
-    return(
-        <Suspense fallback={ <div className="flex items-center justify-center min-h-screen bg-white">
-      <div className="w-12 h-12 border-4 border-gray-300 border-t-[#730B00] rounded-full animate-spin"></div>
-    </div>}>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/comments" element={<App />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/service" element={<Service />} />
-                <Route path="/documentation" element={<Documentation />} />
-            </Routes>
-        </Suspense>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/comments" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/documentation" element={<Documentation />} />
+    </Routes>
+  );
 }
 
 export default AppRoute;
