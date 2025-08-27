a856b4c246ac4db0b464927ce1d758787cd87f45
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-08-27 16:44:39 +0700

    Fix mobile style in Contact page

--- src/pages/contact/ContactForm.jsx
+++ src/pages/contact/ContactForm.jsx
@@ -48,19 +48,19 @@ function ContactForm() {
 
   return (
     <section className="relative lg:h-[500px] md:h-[800px] h-[800px]">
-      <div className="absolute h-full lg:w-[942px] md:w-[600px] sm:w-[600px] bottom-0 transform -translate-x-1/2 left-1/2 right-1/2">
-        <div className="max-w-3x1 mx-auto -mt-20 z-20 relative">
+      <div className="absolute h-full lg:w-[942px] md:w-[600px] w-[600px] bottom-0 transform -translate-x-1/2 left-1/2 right-1/2">
+        <div className="max-sm:max-w-85 mx-auto -mt-20 z-20 relative">
           <div
             ref={formRef} // ✅ target animasi GSAP
-            className="bg-white shadow-xl rounded flex ml-10"
+            className="bg-white shadow-xl rounded flex"
           >
             {/* 🔹 Bagian Form */}
-            <div className="p-10">
-              <h2 className="lg:text-3xl md:text-2xl sm:text-2xl md:text-md sm:text-md font-semibold">
+            <div className="p-8 sm:p-10 max-sm:w-full">
+              <h2 className="lg:text-3xl md:text-2xl sm:text-2xl text-xl font-semibold">
                 Kirimkan pertanyaan mu
               </h2>
               <br />
-              <form className="flex flex-col" onSubmit={handleSubmit}>
+              <form className="flex flex-col lg:w-110 sm:w-130 w-full" onSubmit={handleSubmit}>
                 <div className="mt-1">
                   <label>Nama</label>
                   <input
@@ -77,7 +77,7 @@ function ContactForm() {
                       e.target.setCustomValidity("Harap isi data ini!")
                     }
                     onInput={(e) => e.target.setCustomValidity("")}
-                    className="shadow placeholder:text-[12px] appearance-none border rounded md:w-110 sm:w-120 w-120 py-3 px-3 text-gray-700 leading-tight focus:outline-none mt-1 focus:shadow-outline"
+                    className="shadow placeholder:text-[12px] appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none mt-1 focus:shadow-outline"
                     placeholder="Masukan Nama Mu"
                     required
                   />
@@ -108,7 +108,7 @@ function ContactForm() {
 
                 <button
                   type="submit"
-                  className="bg-white cursor-pointer font-medium text-red-700 mt-5 border-2 border-red-700 px-6 py-2 rounded-lg hover:bg-red-800 hover:text-white ml-75 transition"
+                  className="bg-white cursor-pointer font-medium text-red-700 mt-5 border-2 border-red-700 px-6 py-2 rounded-lg hover:bg-red-800 hover:text-white sm:ml-75 transition"
                 >
                   Kirim pesan
                 </button>
@@ -175,40 +175,57 @@ function ContactForm() {
           </div>
 
           {/* 🔹 Versi Mobile Informasi Kontak */}
-          <div className="lg:hidden block bg-[#A20000] h-80 w-140 mx-10 rounded-b-lg shadow-xl">
+          <div className="lg:hidden block bg-[#A20000] h-fit lg:w-140 rounded-b-lg shadow-xl">
             <div className="mx-10 py-10">
               <h2 className="text-3xl font-semibold text-white text-center mb-2">
                 Informasi Kontak
               </h2>
-              <p className="mb-4 text-[#D3D3D3] text-center text-xl">
+              <p className="mb-4 text-[#D3D3D3] text-center text-lg">
                 Berikut ini adalah kontak kami yang bisa dihubungi
               </p>
               <hr className="border-white mb-2 " />
               <br />
-              <div className="flex ">
-                <img src={telephone} alt="" className="w-8 h-8 mr-5" />
-                <p className="mb-2 text-white font-semibold ">
-                  0813 - 3333 - 0073
-                </p>
-              </div>
-              <div className="flex my-2">
-                <img src={email} alt="" className="w-7 pl-1 mr-3" />
-                <p className="mb-2 text-white">
-                  <a
-                    href="mailto:admin@putraservice.com"
-                    className="underline text-white font-semibold"
-                  >
-                    admin@putraservice.com
+              <div className="flex my-2 items-center">
+                <img src={telephone} alt="" className="w-8 h-8 mr-2" />
+                <p className="mb-1 text-white ">
+                  <a href="https://wa.me/6281333330073">
+                    0813 - 3333 - 0073
                   </a>
                 </p>
               </div>
-              <div className="flex my-2">
-                 <FaInstagram className="text-white size-20"/>
-                  <p>
-                    <a href="https://www.instagram.com/putraservice_/" rel="nofollow" className="underline underline-offset-4 text-white">
-                      @putraservice_
-                    </a>
-                  </p>
+              <div className="flex my-2 items-center">
+                <img src={email} alt="" className="w-7 pl-1 mr-3" />
+                <p className="mb-1 text-white">
+                  <a
+                    href="mailto:pianseptiana@yahoo.com"
+                  >
+                    pianseptiana@yahoo.com
+                  </a>
+                </p>
+              </div>
+              <div className="flex my-2 items-center gap-2">
+                <FaInstagram className="text-white size-8"/>
+                <p>
+                  <a href="https://www.instagram.com/putraservice_/" rel="nofollow" className="  text-white">
+                    @putraservice_
+                  </a>
+                </p>
+              </div>
+              <div className="flex my-2 items-center gap-2">
+                <FaFacebookF className="text-white size-7"/>
+                <p>
+                  <a href="https://www.facebook.com/pisnseptiana/" rel="nofollow" className="  text-white">
+                    @pisnseptiana
+                  </a>
+                </p>
+              </div>
+              <div className="flex my-2 items-center gap-2">
+                <FaXTwitter className="text-white size-7"/>
+                <p>
+                  <a href="https://x.com/putra_service73" rel="nofollow" className="  text-white">
+                    @putra_service73
+                  </a>
+                </p>
               </div>
             </div>
           </div>
