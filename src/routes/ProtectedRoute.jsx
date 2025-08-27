6c8065a6d95261ccedd5c33c760f17ef1c341a29
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-08-27 21:04:46 +0700

    Change AppRoute and ProtectedRoute components with suspense and loading state management

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import LoadingPage from "../components/LoadingPage";
 
 const ProtectedRoute = ({ children }) => {
+  const [authChecked, setAuthChecked] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
     const token = localStorage.getItem("token");
 
-    if(!token) {
-        return <Navigate to="/" replace />;
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
     }
+    setAuthChecked(true); // cek selesai
+  }, []);
 
-    return children;
+  if (!authChecked) {
+    return <LoadingPage />;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
